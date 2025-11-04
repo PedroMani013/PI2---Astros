d0d52b81cd292751a180c3d34f13134efd6ed93b
Pedro Maniezo <maniezopedro45@gmail.com>
2025-11-04 00:31:31 -0300

    adicionando popup de candidatura aprovada

---
+++ popupcandidatura.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASTROS - Sistema De Votação</title>
+    <link rel="shortcut icon" href="images/astros.png">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="tudo">
+        <header class="topo">
+            <img src="images/fatec.png" alt="Logo FATEC" class="logotop">
+            <h1>Votação Para Representante de Sala</h1>
+            <img src="images/cps.png" alt="Logo Cps" class="logotop">
+        </header>
+        <main>
+            <div class="greenpopup">
+                <img src="images/edit.png">
+                <h2>CANDIDATURA APROVADA!</h2>
+                <h3>Um comprovante foi enviado para seu e-mail</h3>
+                <p><a href="votacoesaluno.php">Clique Aqui para voltar para as votações</a></p>
+                <span>.</span>
+            </div> 
+        </main>
+        <footer class="rodape">
+            <img src="images/govsp.png" alt="" class="logosp">
+            <img src="images/astros.png" alt="" class="logobottom">
+        </footer>
+    </div>
+</body>
+
+</html>
